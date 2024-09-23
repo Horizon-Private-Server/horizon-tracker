2fd06d4f5b0c5a1f6abb0d086b663b6dcc179ea4
John Janecek <janecektyler@gmail.com>
2024-09-23 16:05:47 -0600

    Add recency routes

--- horizon/middleware_api.py
+++ horizon/middleware_api.py
@@ -151,7 +151,53 @@ async def get_active_games(protocol: str, host: str, token: str) -> Optional[dic
 
             logger.warning(f"get_active_games got {response.status} from {protocol}://{host}")
             return []
-        
+
+async def get_recent_stats(protocol: str, host: str, token: str, minutes: int=5) -> Optional[dict[str, any]]:
+    """
+    Makes a request to the Horizon Middleware account API which returns
+    a list of [{account_id: {stat_id: stat_value}}]
+
+    :param protocol: "HTTP" or "HTTPS".
+    :param host: Host name and optional port of the target Horizon Middleware server
+        (i.e., "stats.rac-horizon.com" or "111.222.111.222:1234").
+    :param token: The requesting user's authentication token.
+    :param minutes: How many minutes ago to consider 'recent' (max of 60)
+    :return:
+    """
+    headers = {"Authorization": f"Bearer {token}"}
+    active_games_url = f"{protocol}://{host}/Stats/getRecentStatChanges?minutes={minutes}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(active_games_url, headers=headers, ssl=False) as response:
+            if response.status == 200:  # Check if response status code is 200 (OK)
+                return await response.json()  # Parse the response as JSON if status is OK
+
+            logger.warning(f"get_recent_stats got {response.status} from {protocol}://{host}")
+            return []
+
+
+async def get_recent_game_history(protocol: str, host: str, token: str, app_id: int, minutes: int=5) -> Optional[dict[str, any]]:
+    """
+    Makes a request to the Horizon Middleware account API which returns
+    a list of [{account_id: {stat_id: stat_value}}]
+
+    :param protocol: "HTTP" or "HTTPS".
+    :param host: Host name and optional port of the target Horizon Middleware server
+        (i.e., "stats.rac-horizon.com" or "111.222.111.222:1234").
+    :param app_id: The Horizon App ID to filter by.
+    :param token: The requesting user's authentication token.
+    :param minutes: How many minutes ago to consider 'recent' (max of 60)
+    :return:
+    """
+    headers = {"Authorization": f"Bearer {token}"}
+    active_games_url = f"{protocol}://{host}/api/Game/history/getRecentGames?appId={app_id}&minutes={minutes}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(active_games_url, headers=headers, ssl=False) as response:
+            if response.status == 200:  # Check if response status code is 200 (OK)
+                return await response.json()  # Parse the response as JSON if status is OK
+
+            logger.warning(f"get_recent_game_history got {response.status} from {protocol}://{host}")
+            return []
+
 
 
 
@@ -197,3 +243,41 @@ def get_account_basic_stats(protocol: str, host: str, account_id: str | int, app
         return None
 
     return json.loads(account_response.text)
+
+
+def get_all_game_history(protocol: str, host: str, app_id: str | int, token: str, game_end:str = '2024-09-19T15:36:54') -> Optional[dict[str, any]]:
+    """
+    Makes a request to the Horizon Middleware account API which returns
+    all basic stats for a user (including vanilla and custom stats).
+
+    :param protocol: "HTTP" or "HTTPS".
+    :param host: Host name and optional port of the target Horizon Middleware server
+        (i.e., "stats.rac-horizon.com" or "111.222.111.222:1234").
+    :param account_id: The Horizon Account ID to lookup for detailed stats.
+    :param app_id: The Horizon App ID to filter by.
+    :param token: The requesting user's authentication token.
+    :param game_end: The end time for the last record returned
+    :return:
+    """
+    print("Downloading game history ...")
+    counter = 1
+    results = []
+    while True:
+        url = f"{protocol}://{host}/api/Game/historyByDate/{app_id}?lastGameEndDt={game_end}"
+        response = requests.get(url, headers={"Authorization": f"Bearer {token}"}, verify=False)
+
+        if counter % 10 == 0:
+            print(f"Processed {counter*100} games...")
+
+        if response.status_code != 200:
+            logger.warning(f"get_game_history got {response.status_code} from {protocol}://{host}")
+            return results
+
+        result = json.loads(response.text)
+        if len(result) == 0:
+            return results
+        
+        results += result['Games']
+        counter += 1
+
+        game_end = result['NextCursor']
